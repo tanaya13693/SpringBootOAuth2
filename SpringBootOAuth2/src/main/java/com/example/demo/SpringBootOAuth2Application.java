3d236b8439eaf5c2adb026c5b09165af2b002a91
Tanaya Vadgave <tvadgave@deloitte.com>
2019-01-05 17:40:53 -0500

    spring boot starter project

---
+++ SpringBootOAuth2/src/main/java/com/example/demo/SpringBootOAuth2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOAuth2Application.class, args);
+	}
+
+}
+
