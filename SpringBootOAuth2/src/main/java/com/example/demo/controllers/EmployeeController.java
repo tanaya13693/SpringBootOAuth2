ccda838d87e85baaf909360eaad298b3eb5d99c1
Tanaya Vadgave <tvadgave@deloitte.com>
2019-01-05 22:24:02 -0500

    spring boot starter project

---
+++ SpringBootOAuth2/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.entities.Employee;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/user/getEmployeesList", produces = "application/json")
+    @ResponseBody
+    public List<Employee> getEmployeesList() {
+        List<Employee> employees = new ArrayList<>();
+        Employee emp = new Employee();
+        emp.setEmpId("emp1");
+        emp.setEmpName("emp1");
+        employees.add(emp);
+        return employees;
+
+    }
+
+}
